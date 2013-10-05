3799c113954d99d8a5edf94c2153c4c78532bd11
Anthony Brown <bruinbrown@gmail.com>
2013-10-06 00:08:15 +0100

    Added a means to draw our actors and added a couple of textures

--- Platformer/PlatformerGame.fs
+++ Platformer/PlatformerGame.fs
@@ -2,6 +2,7 @@
 
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
+open PlatformerActor
 
 type Game1 () as x =
     inherit Game()
@@ -10,12 +11,25 @@ type Game1 () as x =
     let graphics = new GraphicsDeviceManager(x)
     let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
 
+    let CreateActor' = CreateActor x.Content
+
+    let WorldObjects = lazy ([("player.png", Player(Nothing), Vector2(10.f,28.f), Vector2(32.f,32.f), false);
+                              ("obstacle.png", Obstacle, Vector2(10.f,60.f), Vector2(32.f,32.f), true);
+                              ("", Obstacle, Vector2(42.f,60.f), Vector2(32.f,32.f), true);]
+                             |> List.map CreateActor')
+
+    let DrawActor (sb:SpriteBatch) actor =
+        if actor.Texture.IsSome then 
+            do sb.Draw(actor.Texture.Value, actor.Position, Color.White)
+        ()
+
     override x.Initialize() =
         do spriteBatch <- new SpriteBatch(x.GraphicsDevice)
         do base.Initialize()
         ()
 
     override x.LoadContent() =
+        do WorldObjects.Force () |> ignore
         ()
 
     override x.Update (gameTime) =
@@ -23,4 +37,9 @@ type Game1 () as x =
 
     override x.Draw (gameTime) =
         do x.GraphicsDevice.Clear Color.CornflowerBlue
+        let DrawActor' = DrawActor spriteBatch
+        do spriteBatch.Begin ()
+        WorldObjects.Value
+        |> List.iter DrawActor'
+        do spriteBatch.End ()
         ()
